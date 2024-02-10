ea6ee29088c6450145cb0e42bf22e8658c488afc
WeronikaGRIG <veronikaudot@gmail.com>
2024-02-10 15:29:47 +0100

    changes js

--- script.js
+++ script.js
@@ -4,14 +4,13 @@ const cancelBtn = document.querySelector('.cancel');
 const deleteBtns = document.getElementsByClassName('.delete-note');
 const deleteAllBtn = document.querySelector('.delete-all');
 
-
 const noteAres = document.querySelector('.note-area');
 const notePanel = document.querySelector('.note-panel');
 const category = document.querySelector('#category');
 const textarea = document.querySelector('#text');
 const error = document.querySelector('.error');
 
-let selectValue;
+let selectedValue;
 let cardId = 0;
 
 const openPanel = () => {
@@ -21,9 +20,49 @@ const openPanel = () => {
 const closePanel = () => {
     notePanel.style.display = 'none';
     error.style.visibility = 'hidden';
-    textarea.textContent = '';
+    textarea.value = '';
     category.selectedIndex = 0;
 }
 
+const addNote = () => {
+    if (textarea.value !== '' && category.options[category.selectedIndex].value !== '0') {
+        createNote();
+        error.style.visibility = 'hidden'
+    } else {
+        error.style.visibility = 'visible'
+    }
+}
+
+const createNote = () => {
+    const newNote = document.createElement('div');
+    newNote.classList.add('note');
+    newNote.setAttribute('id', cardId);
+
+    newNote.innerHTML = `
+                <div class="note-header">
+                <h3 class="note-title">${selectedValue}</h3>
+                <button class="delete-note">
+                    <i class="fas fi-times icon"></i>
+                </button>
+            </div>
+
+            <div class="note-body">
+                ${textarea.value}
+            </div>`
+
+    noteAres.appendChild(newNote);
+    cardId++;
+    textarea.value = '';
+    category.selectedIndex = 0;
+    notePanel.style.display = 'none';
+}
+
+const selectValue = () => {
+    selectedValue = category.options[category.selectedIndex].text;
+}
+
+
+
 addBtn.addEventListener('click', openPanel);
 cancelBtn.addEventListener('click', closePanel);
+saveBtn.addEventListener('click', addNote);
